e3f8540e59ed69822bd66495eb788912cc2b201c
yokomotoh <yokomotohama@hotmail.com>
2021-03-29 10:26:44 +0200

    Initial Commit

---
+++ MarketWatch2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarketWatch2
+//
+//  Created by vincent schmitt on 29/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarketWatch2/MarketWatch2App.swift
@@ -0,0 +1,17 @@
+//
+//  MarketWatch2App.swift
+//  MarketWatch2
+//
+//  Created by vincent schmitt on 29/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct MarketWatch2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
